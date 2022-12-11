2db37ffacf424de27fc3a26a35436ab762d1dbf2
Abhishek k <62874675+bobbyy16@users.noreply.github.com>
2022-12-11 13:22:44 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,19 +1,23 @@
+def buildApp() {
+    echo 'building the app in groovy script'
+}
+def buildTest() {
+    echo 'Testing the app in groovy script'
+}
+
+def buildDeploy() {
+    echo 'Deploy the app in groovy script'
+}
 def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
-} 
-
+    echo "building the app"
+    sh "mvn clean package"
+}
 def buildImage() {
-    echo "building the docker image"
-    withCredentials([usernamePassword(credentialsId: 'dockerhubcredential, passwordVariable:'PASS', usernameVariable: 'USER' )]){
-    sh "docker build -t bobbyy16/my-pipeline:1.0 ."
-    sh "echo $PASS | docker login -u $USER --password-stdin"
-    sh "docker push bobbyy16/my-pipeline:1.0"
-   }
-} 
-
-def deployApp() {
-    echo 'deploying the application...'
-} 
-
+     echo "building the docker image"
+     withCredentials([usernamePassword(credentialsId: 'dockerhubcredential, passwordVariable:'PASS', usernameVariable: 'USER' )]){
+     sh "docker build -t jasonkd006/my-repo:${IMAGE_NAME} ."
+     sh "echo $PASS | docker login -u $USER --password-stdin"
+     sh "docker push jasonkd006/my-repo:${IMAGE_NAME}"
+     }
+}
 return this
